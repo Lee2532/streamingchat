85327c95b7fe31db98bbcdfc774b2959070fc300
Lee2523 <seungoh0423@gmail.com>
2023-02-20 23:43:57 +0900

    pre-commit 적용

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,11 +1,13 @@
 import pytest
 
+
 def increment(x):
     return x + 1
 
+
 def test_increment():
-    for i in range(10):
-        print(i)
+    print("test code run")
     assert increment(3) == 4
     assert increment(0) == 1
     assert increment(-1) == 0
+    print("test code exit")
