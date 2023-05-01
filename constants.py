5be34dba3d103551571eacdb68ee637e1d4790d3
Lee2523 <seungoh0423@gmail.com>
2023-05-01 22:11:19 +0900

    kafka config constants로 뺌

--- constants.py
+++ constants.py
@@ -1,3 +1,8 @@
 BOOTSTRAPSERVER = "127.0.0.1:9092"
 SCHEMA_REGISTRY_URL = "http://127.0.0.1:8081"
 CLIENTID = "twitch-producer"
+ACKS = 1
+KAFKA_CONFIG = {
+    "bootstrap.servers": BOOTSTRAPSERVER,
+    "acks": ACKS,
+}
